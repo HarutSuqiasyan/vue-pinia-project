cc749a37c87d911470bee7088380bcb911f5357b
HarutSuqiasyan <harutsuqiasyan00@gmail.com>
2025-09-13 22:40:34 +0400

    add fetch

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,14 @@
       <h2>My Favorite Movies</h2>
     </header>
     <div class="tabs">
-      <button :class="['btn', { btn_green: movieStore.activeTab === 1 }]">
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 1 }]"
+        @click="setTab(1)">
         Favorite
       </button>
-      <button :class="['btn', { btn_green: movieStore.activeTab === 2 }]">
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 2 }]"
+        @click="setTab(2)">
         Search
       </button>
     </div>
@@ -26,13 +30,20 @@
         :key="movie.id"
         :movie="movie" />
     </div>
-    <div class="search" v-else>Search</div>
+    <div class="search" v-else>
+      <Search />
+    </div>
   </main>
 </template>
 
 <script setup>
+import Search from "./components/Search.vue";
 import Movie from "./components/Movie.vue";
 import { useMovieStore } from "./stores/movieStore";
+
+const setTab = (id) => {
+  movieStore.setActiveTab(id);
+};
 
 const movieStore = useMovieStore();
 </script>
